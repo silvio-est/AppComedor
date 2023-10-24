36c5dedd6249cedb24ef4e0d73715a0694135bef
Silvio Estevan Montalvo <msilvioesteban@gmail.com>
2023-10-24 13:29:34 -0400

    Inicio del Proyecto

---
+++ src/main/java/com/unah/appcomedor/AppComedor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unah.appcomedor;
+
+/**
+ *
+ * @author msilv
+ */
+public class AppComedor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
